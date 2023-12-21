d24de0aff7c08fa70f4777c1e16d91e5e65b3d99
Aris Ripandi <riipandi@gmail.com>
2023-12-21 20:55:38 +0700

    initial commit

---
+++ .eleventy.js
@@ -0,0 +1,139 @@
+const markdownIt = require('markdown-it')
+const markdownItAnchor = require('markdown-it-anchor')
+
+const EleventyPluginNavigation = require('@11ty/eleventy-navigation')
+const EleventyPluginRss = require('@11ty/eleventy-plugin-rss')
+const EleventyPluginSyntaxhighlight = require('@11ty/eleventy-plugin-syntaxhighlight')
+const EleventyVitePlugin = require('@11ty/eleventy-plugin-vite')
+
+const rollupPluginCritical = require('rollup-plugin-critical').default
+
+const filters = require('./utils/filters.js')
+const transforms = require('./utils/transforms.js')
+const shortcodes = require('./utils/shortcodes.js')
+
+const { resolve } = require('path')
+
+module.exports = function (eleventyConfig) {
+	eleventyConfig.setServerPassthroughCopyBehavior('copy')
+	eleventyConfig.addPassthroughCopy('public')
+
+	// Plugins
+	eleventyConfig.addPlugin(EleventyPluginNavigation)
+	eleventyConfig.addPlugin(EleventyPluginRss)
+	eleventyConfig.addPlugin(EleventyPluginSyntaxhighlight)
+	eleventyConfig.addPlugin(EleventyVitePlugin, {
+		tempFolderName: '_tmp', // Default name of the temp folder
+
+		// Vite options (equal to vite.config.js inside project root)
+		viteOptions: {
+			publicDir: 'public',
+			clearScreen: false,
+			server: {
+				mode: 'development',
+				middlewareMode: true
+			},
+			appType: 'custom',
+			assetsInclude: ['**/*.xml', '**/*.txt'],
+			build: {
+				mode: 'production',
+				sourcemap: 'true',
+				manifest: true,
+				// This puts CSS and JS in subfolders – remove if you want all of it to be in /assets instead
+				rollupOptions: {
+					output: {
+						assetFileNames: 'assets/css/main.[hash].css',
+						chunkFileNames: 'assets/js/[name].[hash].js',
+						entryFileNames: 'assets/js/[name].[hash].js'
+					},
+					plugins: [
+						rollupPluginCritical({
+							criticalUrl: './_site/',
+							criticalBase: './_site/',
+							criticalPages: [
+								{ uri: 'index.html', template: 'index' },
+								{ uri: 'posts/index.html', template: 'posts/index' },
+								{ uri: '404.html', template: '404' }
+							],
+							criticalConfig: {
+								inline: true,
+								dimensions: [
+									{
+										height: 900,
+										width: 375
+									},
+									{
+										height: 720,
+										width: 1280
+									},
+									{
+										height: 1080,
+										width: 1920
+									}
+								],
+								penthouse: {
+									forceInclude: ['.fonts-loaded-1 body', '.fonts-loaded-2 body']
+								}
+							}
+						})
+					]
+				}
+			}
+		}
+	})
+
+	// Filters
+	Object.keys(filters).forEach((filterName) => {
+		eleventyConfig.addFilter(filterName, filters[filterName])
+	})
+
+	// Transforms
+	Object.keys(transforms).forEach((transformName) => {
+		eleventyConfig.addTransform(transformName, transforms[transformName])
+	})
+
+	// Shortcodes
+	Object.keys(shortcodes).forEach((shortcodeName) => {
+		eleventyConfig.addShortcode(shortcodeName, shortcodes[shortcodeName])
+	})
+
+	eleventyConfig.addShortcode('year', () => `${new Date().getFullYear()}`)
+
+	// Customize Markdown library and settings:
+	let markdownLibrary = markdownIt({
+		html: true,
+		breaks: true,
+		linkify: true
+	}).use(markdownItAnchor, {
+		permalink: markdownItAnchor.permalink.ariaHidden({
+			placement: 'after',
+			class: 'direct-link',
+			symbol: '#',
+			level: [1, 2, 3, 4]
+		}),
+		slugify: eleventyConfig.getFilter('slug')
+	})
+	eleventyConfig.setLibrary('md', markdownLibrary)
+
+	// Layouts
+	eleventyConfig.addLayoutAlias('base', 'base.njk')
+	eleventyConfig.addLayoutAlias('post', 'post.njk')
+
+	// Copy/pass-through files
+	eleventyConfig.addPassthroughCopy('src/assets/css')
+	eleventyConfig.addPassthroughCopy('src/assets/js')
+
+	return {
+		templateFormats: ['md', 'njk', 'html', 'liquid'],
+		htmlTemplateEngine: 'njk',
+		passthroughFileCopy: true,
+		dir: {
+			input: 'src',
+			// better not use "public" as the name of the output folder (see above...)
+			output: '_site',
+			includes: '_includes',
+			layouts: 'layouts',
+			data: '_data'
+		}
+	}
+}
---
+++ README.md
@@ -0,0 +1,80 @@
+# v60 - Eleventy Vite Starter
+
+<!-- [![Netlify Status](https://api.netlify.com/api/v1/badges/ef99b4ea-199f-497b-84c1-48c34355da8a/deploy-status)](https://app.netlify.com/sites/eleventyplusvite/deploys) -->
+
+A clean and fast Eleventy Starter Project with Vite.
+
+## Features
+
+- Eleventy 2.0.1
+- New Eleventy 2.0 Dev Server with live reload
+- Vite 5.0.10
+- Vite as Middleware in Eleventy Dev Server (uses [eleventy-plugin-vite](https://github.com/11ty/eleventy-plugin-vite/))
+- Eleventy build output is post-processed by [Vite](https://vitejs.dev) (with Rollup)
+- CSS/Sass post-processing with PostCSS incl. [Autoprefixer](https://github.com/postcss/autoprefixer) and cssnano
+- Uses [my own opinionated CSS/Sass structure](https://matthiasott.com/notes/how-i-structure-my-css)
+- Critical CSS, generated and inlined via [rollup-plugin-critical](https://github.com/nystudio107/rollup-plugin-critical). The main CSS file is then loaded asynchronously with [Scott Jehl’s `media` loading strategy](https://www.filamentgroup.com/lab/load-css-simpler/) (adds `media="print"` and swaps to `media="all"` once loaded)
+- Example implementation of a web font loading strategy ([critical FOFT with preload](https://www.zachleat.com/web/comprehensive-webfonts/#critical-foft-preload))
+- Basic fluid typography based on [Utopia](https://utopia.fyi)
+- Basic dark mode support (using `prefers-color-scheme` and CSS Custom Properties)
+- Polyfill for [focus-visible](https://matthiasott.com/notes/focus-visible-is-here)
+- RSS feed 🧡
+- XML sitemap
+- Four Hundos Lighthouse score 💯💯💯💯
+
+## Getting started
+
+Start by [generating a new repository based on this project](https://github.com/riipandi/v60/generate).
+
+After cloning (or downloading) the repository to your local machine, install all dependencies with the command
+
+```sh
+pnpm install
+```
+
+## Run dev server
+
+The project comes with Eleventy’s built-in development server. You can start the server with
+
+```sh
+pnpm serve
+```
+
+## Build
+
+To trigger a production build, use
+
+```sh
+pnpm build
+```
+
+## Deploy a fork of this template to Netlify
+
+[![Deploy to Netlify](https://www.netlify.com/img/deploy/button.svg)](https://app.netlify.com/start/deploy?repository=https://github.com/riipandi/v60)
+
+## CSS
+
+By default, this starter project uses Sass with an opinionated folder structure. Feel free to replace this structure with your own. If you prefer to write standards-compliant, good old plain CSS, this is also supported. Nesting is then possible via the [PostCSS Nesting plugin](https://github.com/csstools/postcss-plugins/tree/main/plugins/postcss-nesting), following the [CSS Nesting specification](https://drafts.csswg.org/css-nesting-1/).
+
+[Autoprefixer](https://github.com/postcss/autoprefixer) adds necessary browser prefixes. The [browserslist](https://github.com/browserslist/browserslist) settings can be adjusted in `package.json`.
+
+## Roadmap
+
+- Add more base styles and a demo page that shows example styles and components
+- Add a toggle button for the dark mode theme
+- More advanced base styles for modern CSS layout
+- Webmention/IndieWeb support
+
+## Feedback
+
+Please provide feedback! 🤗 Ideally by [filing an issue here](https://github.com/riipandi/v60/issues) – or via a pull request.
+
+## Thank you!
+
+This starter project would not have been possible without the many great sites and projects I was able to learn from, use as inspiration, and shamelessly copy code from:
+
+- Matthias Ott [matthiasott/eleventy-plus-vite](https://github.com/matthiasott/eleventy-plus-vite)
+- Zach Leatherman [zachleat.com](https://github.com/zachleat/zachleat.com)
+- Max Böck’s [Eleventastic](https://github.com/maxboeck/eleventastic)
+- Stephanie Eckles’s [11ty Netlify Jumpstart](https://github.com/5t3ph/11ty-netlify-jumpstart)
+- Miriam Suzanne [miriamsuzanne.com](https://www.miriamsuzanne.com)
